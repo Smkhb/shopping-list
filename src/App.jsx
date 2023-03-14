a54bfb6531025c798d1f8865d4fc5c5b6ae9daa4
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-03-14 14:42:35 -0300

    Lista sem Estilo

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,44 @@ function App() {
   return (
     <main className="bg-gradient-to-r from-moonlitAsteroid-100 via-moonlitAsteroid-200 to-moonlitAsteroid-300">
       <section className='min-h-screen flex justify-center items-center'>
-      <h1>Hello World</h1>
+
+        <div>
+          <input placeholder='Add an item...' />
+          <FaPlus />
+        </div>
+
+        <div>
+          <div>
+
+            <div>
+              {false ? (
+                <>
+                  <FaCheckCircle />
+                  <span>Item 1</span>
+                </>
+              ) : (
+                <>
+                  <FaCircle />
+                  <span>Item 1</span>
+                </>
+              )}
+            </div>
+
+            <div>
+              <button>
+                <FaChevronLeft />
+              </button>
+              <span> 1 </span>
+              <button>
+                <FaChevronRight />
+              </button>
+            </div>
+
+          </div>
+        </div>
+
+        <div>Total: 6</div>
+
       </section>
     </main>
   )
