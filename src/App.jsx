9b123536dddb822e585ecd9200ec8dfcb5f37e7f
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-03-14 17:16:50 -0300

    estilizado

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,23 @@ import { FaCheckCircle, FaCircle, FaPlus, FaChevronLeft, FaChevronRight } from "
 function App() {
 
   return (
-    <main className="bg-gradient-to-r from-moonlitAsteroid-100 via-moonlitAsteroid-200 to-moonlitAsteroid-300">
+    <main className="bg-gradient-to-r from-moonlitAsteroid-100 via-moonlitAsteroid-200 to-moonlitAsteroid-300 text-slate-200 text-xl">
       <section className='min-h-screen flex justify-center items-center'>
-
-        <div>
-          <div>
-            <input placeholder='Add an item...' />
-            <FaPlus />
+        
+        {/* Div Principal */}
+        <div className=' bg-cyan-700 py-8 px-4 rounded-md shadow-xl'>
+          
+          {/* Campo de Texto:Adicionar Item */}
+          <div className='flex bg-gradient-to-r from-sky-300 to-blue-300 rounded-md py-2 px-4 mb-4'>
+            <input className=' placeholder:text-slate-200 placeholder:font-bold bg-transparent  outline-none' placeholder='Adicionar Item. . .' />
+            <FaPlus className=' self-center' />
           </div>
-
-          <div>
-            <div>
-
-              <div>
+            
+            {/* Item */}
+            <div className='flex items-center justify-between border-b-2 border-dashed'>
+              
+              {/* Circulo e item */}
+              <div  className='flex py-2  gap-2 items-center'>
                 {false ? (
                   <>
                     <FaCheckCircle />
@@ -29,8 +33,10 @@ function App() {
                   </>
                 )}
               </div>
-
-              <div>
+              
+              {/* Setas e Numero */}
+              <div className='bg-slate-200 text-cyan-700 rounded-2xl flex items-center gap-1 '>
+                
                 <button>
                   <FaChevronLeft />
                 </button>
@@ -38,14 +44,14 @@ function App() {
                 <button>
                   <FaChevronRight />
                 </button>
+
               </div>
 
             </div>
-          </div>
 
-          <div>Total: 6</div>
+          {/* Total */}
+          <div className='text-end pt-2'>Total: 6</div>
         </div>
-
 
       </section>
     </main>
